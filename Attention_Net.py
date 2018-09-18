e2904c8a736a7b0ffc8701897170c439e9ad7748
Wang Zilong <Wang_Zilong_CN@outlook.com>
2018-09-18 16:54:03 +0800

    ignore try

---
+++ Attention_Net.py
@@ -0,0 +1,506 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+from Net import BiLstmSentenceEncoder
+
+GPU = False
+
+# Modules
+class Linear(nn.Module):
+    ''' Simple Linear layer with xavier init '''
+    def __init__(self, d_in, d_out, bias=True):
+        super(Linear, self).__init__()
+        self.linear = nn.Linear(d_in, d_out, bias=bias)
+        init.xavier_normal_(self.linear.weight)
+
+    def forward(self, x):
+        return self.linear(x)
+
+class Bottle(nn.Module):
+    ''' Perform the reshape routine before and after an operation '''
+
+    def forward(self, input):
+        if len(input.size()) <= 2:
+            return super(Bottle, self).forward(input)
+        size = input.size()[:2]
+        out = super(Bottle, self).forward(input.view(size[0]*size[1], -1))
+        return out.view(size[0], size[1], -1)
+
+class BottleLinear(Bottle, Linear):
+    ''' Perform the reshape routine before and after a linear projection '''
+    pass
+
+class BottleSoftmax(Bottle, nn.Softmax):
+    ''' Perform the reshape routine before and after a softmax operation'''
+    pass
+
+class ScaledDotProductAttention(nn.Module):
+
+    def __init__(self, model_dim, attention_dropout=0.1):
+        super(ScaledDotProductAttention, self).__init__()
+        self.scale_constant = np.power(model_dim, 0.5)
+        self.dropout_layer = nn.Dropout(attention_dropout)
+        self.softmax_layer = BottleSoftmax()
+
+    def forward(self, q, k, v, mask=None):
+        attention_matrix = torch.mm(q, k.transpose(0, 1)) / self.scale_constant
+
+        if mask is not None:
+            attention_matrix.data.masked_fill_(mask, -float('inf'))
+
+        attention_matrix = self.softmax_layer(attention_matrix)
+        attention_matrix = self.dropout_layer(attention_matrix)
+
+        output = torch.mm(attention_matrix, v)
+
+        return output
+
+class ScaledDotProductAttention_Batch(nn.Module):
+
+    def __init__(self, model_dim, attention_dropout=0.1):
+        super(ScaledDotProductAttention_Batch, self).__init__()
+        self.scale_constant = np.power(model_dim, 0.5)
+        self.dropout_layer = nn.Dropout(attention_dropout)
+        self.softmax_layer = BottleSoftmax()
+
+    def forward(self, q, k, v, mask=None):
+        attention_matrix = torch.bmm(q, k.transpose(1, 2)) / self.scale_constant
+
+        if mask is not None:
+            attention_matrix.data.masked_fill_(mask, -float('inf'))
+
+        attention_matrix = self.softmax_layer(attention_matrix)
+        attention_matrix = self.dropout_layer(attention_matrix)
+
+        output = torch.bmm(attention_matrix, v)
+
+        return output
+
+class LayerNormalization(nn.Module):
+    def __init__(self, d_hid, eps=1e-3):
+        super(LayerNormalization, self).__init__()
+
+        self.eps = eps
+        self.a_2 = nn.Parameter(torch.ones(d_hid), requires_grad=True)
+        self.b_2 = nn.Parameter(torch.zeros(d_hid), requires_grad=True)
+
+    def forward(self, z):
+        if z.size(1) == 1:
+            return z
+
+        mu = torch.mean(z, keepdim=True, dim=-1)
+        sigma = torch.std(z, keepdim=True, dim=-1)
+        ln_out = (z - mu.expand_as(z)) / (sigma.expand_as(z) + self.eps)
+        ln_out = ln_out * self.a_2.expand_as(ln_out) + self.b_2.expand_as(ln_out)
+
+        return ln_out
+
+# Sublayer
+class MultiHeadAttentionUnit(nn.Module):
+
+    def __init__(self, head_num, model_dim, k_dim, v_dim, dropout):
+        super(MultiHeadAttentionUnit, self).__init__()
+
+        self.head_num = head_num
+        self.k_dim = k_dim
+        self.v_dim = v_dim
+
+        self.w_qs = nn.Parameter(torch.FloatTensor(head_num, model_dim, k_dim))
+        self.w_ks = nn.Parameter(torch.FloatTensor(head_num, model_dim, k_dim))
+        self.w_vs = nn.Parameter(torch.FloatTensor(head_num, model_dim, v_dim))
+
+        self.attention_layer = ScaledDotProductAttention_Batch(model_dim)
+        self.norm_layer = LayerNormalization(model_dim)
+        self.linear_layer = BottleLinear(head_num*v_dim, model_dim) # nn.Linear(head_num * v_dim, model_dim, bias=True)
+
+        self.dropout_layer = nn.Dropout(dropout)
+
+        init.xavier_normal_(self.w_qs)
+        init.xavier_normal_(self.w_ks)
+        init.xavier_normal_(self.w_vs)
+
+    def forward(self, q, k, v, attention_mask=None):
+        k_dim = self.k_dim
+        v_dim = self.v_dim
+        head_num = self.head_num
+
+        res = q
+
+        batch_size, q_len, model_dim = q.size()
+        batch_size, k_len, model_dim = k.size()
+        batch_size, v_len, model_dim = v.size()
+
+        multi_q = q.repeat(head_num, 1, 1).view(head_num, -1, model_dim)
+        multi_k = k.repeat(head_num, 1, 1).view(head_num, -1, model_dim)
+        multi_v = v.repeat(head_num, 1, 1).view(head_num, -1, model_dim)
+
+        multi_q = torch.bmm(multi_q, self.w_qs).view(-1, q_len, k_dim)
+        multi_k = torch.bmm(multi_k, self.w_ks).view(-1, k_len, k_dim)
+        multi_v = torch.bmm(multi_v, self.w_vs).view(-1, v_len, v_dim)
+
+        if attention_mask is not None:
+            attention_mask = attention_mask.repeat(head_num, 1, 1)
+
+        attention_output = self.attention_layer(multi_q, multi_k, multi_v,
+                                                mask=attention_mask)
+
+        tmp_output = torch.cat(torch.split(attention_output, batch_size, dim=0), dim=-1)
+
+        linear_output = self.linear_layer(tmp_output)
+
+        dropout_output = self.dropout_layer(linear_output)
+
+        return self.norm_layer(dropout_output + res)
+
+class OneHeadAttentionUnit(nn.Module):
+
+    def __init__(self, model_dim, k_dim, v_dim, dropout=0.1):
+        super(OneHeadAttentionUnit, self).__init__()
+
+        self.k_dim = k_dim
+        self.v_dim = v_dim
+
+        self.w_qs = nn.Parameter(torch.FloatTensor(model_dim, k_dim))
+        self.w_ks = nn.Parameter(torch.FloatTensor(model_dim, k_dim))
+        self.w_vs = nn.Parameter(torch.FloatTensor(model_dim, v_dim))
+
+        self.attention_layer = ScaledDotProductAttention(model_dim)
+        self.norm_layer = LayerNormalization(model_dim)
+        self.dropout_layer = nn.Dropout(dropout)
+
+        init.xavier_normal_(self.w_qs)
+        init.xavier_normal_(self.w_ks)
+        init.xavier_normal_(self.w_vs)
+
+    def forward(self, q, k, v, attention_mask=None):
+
+        res = q
+
+        multi_q = torch.mm(q, self.w_qs)
+        multi_k = torch.mm(k, self.w_ks)
+        multi_v = torch.mm(v, self.w_vs)
+
+        attention_output = self.attention_layer(multi_q, multi_k, multi_v,
+                                                mask=attention_mask)
+
+        dropout_output = self.dropout_layer(attention_output)
+
+        return self.norm_layer(dropout_output + res)
+
+class PositionwiseFeedForward(nn.Module):
+
+    def __init__(self, hid_dim, inner_hid_dim, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Conv1d(hid_dim, inner_hid_dim, 1) # position-wise
+        self.w_2 = nn.Conv1d(inner_hid_dim, hid_dim, 1) # position-wise
+        self.layer_norm = LayerNormalization(hid_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        output = self.relu(self.w_1(x.transpose(1, 2)))
+        output = self.w_2(output).transpose(2, 1)
+        output = self.dropout(output)
+        return self.layer_norm(output + residual)
+
+# Layer
+class EncoderLayer(nn.Module):
+
+    def __init__(self, model_dim, inner_hid_dim, head_num, k_dim, v_dim, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+
+        self.attention_layer = MultiHeadAttentionUnit(
+            head_num=head_num,
+            model_dim=model_dim,
+            k_dim=k_dim,
+            v_dim=v_dim,
+            dropout=dropout
+        )
+
+        self.feedforward_layer = PositionwiseFeedForward(
+            hid_dim=model_dim,
+            inner_hid_dim=inner_hid_dim,
+            dropout=dropout
+        )
+
+    def forward(self, input, attention_mask=None):
+        attention_output = self.attention_layer(
+            q=input,
+            k=input,
+            v=input,
+            attention_mask=attention_mask
+        )
+        feedforward_output = self.feedforward_layer(attention_output)
+        return feedforward_output
+
+# Model
+def position_encoding_init(position_num, pos_vec_dim):
+    ''' Init the sinusoid position encoding table '''
+
+    # keep dim 0 for padding token position encoding zero vector
+    position_enc = np.array([
+        [pos / np.power(10000, 2 * (j // 2) / pos_vec_dim) for j in range(pos_vec_dim)]
+        if pos != 0 else np.zeros(pos_vec_dim) for pos in range(position_num)])
+
+    position_enc[1:, 0::2] = np.sin(position_enc[1:, 0::2]) # dim 2i
+    position_enc[1:, 1::2] = np.cos(position_enc[1:, 1::2]) # dim 2i+1
+    return torch.from_numpy(position_enc).type(torch.FloatTensor)
+
+def get_attention_padding_mask(seq_q, seq_k):
+    ''' Indicate the padding-related part to mask '''
+    assert seq_q.dim() == 2 and seq_k.dim() == 2
+    mb_size, len_q = seq_q.size()
+    mb_size, len_k = seq_k.size()
+    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)   # bx1xsk
+    pad_attn_mask = pad_attn_mask.expand(mb_size, len_q, len_k) # bxsqxsk
+    return pad_attn_mask
+
+class Encoder(nn.Module):
+
+    def __init__(self, vocab_size, sentence_length, layer_num, head_num, k_dim, v_dim,
+                 word_vec_dim, model_dim, inner_hid_dim, word_vec_matrix, dropout=0.1):
+        super(Encoder, self).__init__()
+
+        position_num = sentence_length + 1
+        self.sentence_length = sentence_length
+        self.model_dim = model_dim
+
+        self.position_embedding = nn.Embedding(position_num, word_vec_dim, padding_idx=0)
+        self.position_embedding.weight.data = position_encoding_init(position_num, word_vec_dim)
+
+        self.word_embedding = nn.Embedding(vocab_size, word_vec_dim, padding_idx=0)
+        self.word_embedding.weight.data = torch.from_numpy(word_vec_matrix)
+
+        self.layer_stack = nn.ModuleList(
+            [EncoderLayer(model_dim=model_dim,
+                          inner_hid_dim=inner_hid_dim,
+                          head_num=head_num,
+                          k_dim=k_dim,
+                          v_dim=v_dim,
+                          dropout=dropout)
+             for _ in range(layer_num)
+             ]
+        )
+
+    def forward(self, word_seq, pos_seq):
+        word_embedding_output = self.word_embedding(word_seq)
+        position_embedding_output = self.position_embedding(pos_seq)
+
+        embedding_output = word_embedding_output.float() + position_embedding_output.float()
+
+        output = embedding_output
+        attention_mask = get_attention_padding_mask(word_seq, word_seq)
+
+        for encoder_layer in self.layer_stack:
+            output = encoder_layer(output, attention_mask)
+
+        return output
+
+class Encoder_Max_Pooling(nn.Module):
+
+    def __init__(self, vocab_size, sentence_length, layer_num, head_num, k_dim, v_dim,
+                 word_vec_dim, model_dim, inner_hid_dim, word_vec_matrix, dropout=0.1):
+
+        super(Encoder_Max_Pooling, self).__init__()
+
+        self.Encoder = Encoder(vocab_size, sentence_length, layer_num, head_num, k_dim, v_dim,
+                               word_vec_dim, model_dim, inner_hid_dim, word_vec_matrix, dropout)
+
+    def forward(self, word_seq, pos_seq):
+        output = self.Encoder(word_seq, pos_seq)
+        max_pooling_output = torch.max(output, 1)[0]
+        return max_pooling_output
+
+class Sent_Encoder(nn.Module):
+    def __init__(self, paragraph_length, layer_num, head_num, k_dim, v_dim,
+                 input_vec_dim, model_dim, inner_hid_dim, dropout=0.1):
+        super(Sent_Encoder, self).__init__()
+
+        position_num = paragraph_length + 1
+        self.paragraph_length = paragraph_length
+        self.model_dim = model_dim
+
+        self.position_embedding = nn.Embedding(position_num, input_vec_dim, padding_idx=0)
+        self.position_embedding.weight.data = position_encoding_init(position_num, input_vec_dim)
+
+        self.layer_stack = nn.ModuleList(
+            [EncoderLayer(model_dim=model_dim,
+                          inner_hid_dim=inner_hid_dim,
+                          head_num=head_num,
+                          k_dim=k_dim,
+                          v_dim=v_dim,
+                          dropout=dropout)
+             for _ in range(layer_num)
+             ]
+        )
+
+    def forward(self, para_matrix):
+        pos_seq = torch.tensor([i for i in range(1, self.paragraph_length+1)]).cuda()
+
+        if GPU:
+            pos_seq = pos_seq.cuda()
+
+        position_embedding_output = self.position_embedding(pos_seq)
+
+        embedding_output = para_matrix.float() + position_embedding_output.float()
+
+        output = embedding_output
+
+        for encoder_layer in self.layer_stack:
+            output = encoder_layer(output)
+
+        return output
+
+class TransformerEncoder_BiLSTM(nn.Module):
+
+    def __init__(self, encoder_vocab_size, encoder_sentence_length, encoder_layer_num, encoder_head_num, encoder_k_dim,
+                 encoder_v_dim, encoder_word_vec_dim, encoder_model_dim, encoder_inner_hid_dim, word_vec_matrix,
+                 sent_hidden_dim, sent_fc_dim, sent_dropout, tagset_size):
+
+        super(TransformerEncoder_BiLSTM, self).__init__()
+
+        self.sentence_encoder = Encoder_Max_Pooling(vocab_size=encoder_vocab_size,
+                                                    sentence_length=encoder_sentence_length,
+                                                    layer_num=encoder_layer_num,
+                                                    head_num=encoder_head_num,
+                                                    k_dim=encoder_k_dim,
+                                                    v_dim=encoder_v_dim,
+                                                    word_vec_dim=encoder_word_vec_dim,
+                                                    model_dim=encoder_model_dim,
+                                                    inner_hid_dim=encoder_inner_hid_dim,
+                                                    word_vec_matrix=word_vec_matrix)
+
+        self.sent_lstm = nn.LSTM(input_size=encoder_model_dim, hidden_size=sent_hidden_dim, bidirectional=True, batch_first=True)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2 * sent_hidden_dim, sent_fc_dim),
+            nn.Dropout(sent_dropout),
+            nn.ReLU(),
+            nn.Linear(sent_fc_dim, sent_fc_dim),
+            nn.Dropout(sent_dropout),
+            nn.ReLU(),
+            nn.Linear(sent_fc_dim, tagset_size)
+        )
+
+    def forward(self, word_seq, pos_seq):
+        sentence_encoder_out = self.sentence_encoder(word_seq, pos_seq)
+        sent_lstm_out, _ = self.sent_lstm(sentence_encoder_out.view(len(sentence_encoder_out), 1, -1))
+        tag_space = self.classifier(sent_lstm_out.view(len(sent_lstm_out), -1))
+        return tag_space
+
+class BiLSTM_TransformerEncoder(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, fc_dim, vocab_size, tagset_size, word_vec_matrix, dropout,
+                 paragraph_length, layer_num, head_num, k_dim, v_dim, input_vec_dim, model_dim, inner_hid_dim):
+        super(BiLSTM_TransformerEncoder, self).__init__()
+
+        self.sentence_encoder = BiLstmSentenceEncoder(embedding_dim=embedding_dim,
+                                                      hidden_dim=hidden_dim,
+                                                      vocab_size=vocab_size,
+                                                      word_vec_matrix=word_vec_matrix)
+
+        self.sent_transformer = Sent_Encoder(paragraph_length=paragraph_length,
+                                             layer_num=layer_num,
+                                             head_num=head_num,
+                                             k_dim=k_dim,
+                                             v_dim=v_dim,
+                                             inner_hid_dim=inner_hid_dim,
+                                             input_vec_dim=input_vec_dim,
+                                             model_dim=model_dim)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(model_dim, fc_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(fc_dim, fc_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(fc_dim, tagset_size)
+        )
+
+    def forward(self, sentence_tuple):
+        sentence_encoder_out = self.sentence_encoder(sentence_tuple)
+        tranformer_out = self.sent_transformer(sentence_encoder_out.view(1, sentence_encoder_out.shape[0], -1))
+        tag_space = self.classifier(tranformer_out.view(tranformer_out.shape[1], -1))
+        return tag_space
+
+class BiLSTM_Attention(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, fc_dim, vocab_size, tagset_size, word_vec_matrix, dropout,
+                 max_paragraph_len):
+        super(BiLSTM_Attention, self).__init__()
+
+        self.sentence_encoder = BiLstmSentenceEncoder(embedding_dim=embedding_dim,
+                                                      hidden_dim=hidden_dim,
+                                                      vocab_size=vocab_size,
+                                                      word_vec_matrix=word_vec_matrix)
+
+        self.model_dim = 2 * embedding_dim
+
+        self.position_num = max_paragraph_len
+        self.position_embedding = nn.Embedding(max_paragraph_len + 1, self.model_dim, padding_idx=0)
+        self.position_embedding.weight.data = position_encoding_init(max_paragraph_len, self.model_dim)
+
+        self.attention = OneHeadAttentionUnit(self.model_dim, self.model_dim, self.model_dim)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.model_dim, fc_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(fc_dim, fc_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(fc_dim, tagset_size)
+        )
+
+    def forward(self, sentence_tuple):
+        sentence_encoder_out = self.sentence_encoder(sentence_tuple)
+
+        paragragh_len = len(sentence_tuple[0])
+        pos_list = torch.LongTensor([i for i in range(paragragh_len)])
+
+        if GPU:
+            pos_list = pos_list.cuda()
+
+        pos_embedding = self.position_embedding(pos_list)
+
+        attention_input = sentence_encoder_out + pos_embedding
+
+        pad = nn.ZeroPad2d((0, 0, 0, self.position_num - paragragh_len))
+        attention_input = pad(attention_input)
+
+        len_mask = np.ones((self.position_num, 1))
+        len_mask[paragragh_len:] = 0
+        attn_mask = np.matmul(len_mask, len_mask.transpose())
+        attn_mask = torch.from_numpy(attn_mask)
+
+        if GPU:
+            attn_mask = attn_mask.cuda()
+
+        attn_mask = torch.eq(attn_mask, 0)
+
+        attention_out = self.attention(attention_input, attention_input, attention_input, attn_mask)
+
+        tag_space = self.classifier(attention_out)
+
+        tag_space = tag_space[:paragragh_len]
+
+        return tag_space
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Net.py
@@ -0,0 +1,143 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+
+# Net
+class BiLstmSentenceEncoder(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, word_vec_matrix):
+        super(BiLstmSentenceEncoder, self).__init__()
+
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.word_embeddings.weight.data.copy_(torch.from_numpy(word_vec_matrix))
+
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=True, batch_first=True)
+
+    def forward(self, sentence_tuple):
+        # split input
+        sentence = sentence_tuple[0]
+        sentence_length_list = sentence_tuple[1]
+
+        # eliminate extra zeros
+        max_len = int(sentence_length_list.max())
+        sentence = sentence[:, 0:max_len]
+
+        # get word embedding
+        embeds = self.word_embeddings(sentence)
+
+        # sort
+        sentence_length_list, indices = torch.sort(sentence_length_list, descending=True)
+        _, desorted_indices = torch.sort(indices, descending=False)
+
+        embeds = embeds[indices]
+
+        embeds = pack_padded_sequence(embeds, sentence_length_list.cpu().numpy(), batch_first=True)
+        lstm_out, _ = self.lstm(embeds)
+        lstm_out, _ = pad_packed_sequence(lstm_out, batch_first=True)
+
+        # unsort
+        lstm_out = lstm_out[desorted_indices]
+
+        # max_pooling
+        max_pooling_out = torch.max(lstm_out, 1)[0]
+
+        #  don't know why in emotionX: the axis = 1 ?????
+        # max_pooling_out = F.max_pool1d(lstm_out, kernel_size=600)
+
+        return max_pooling_out
+
+class BiLSTM_BiLSTM(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, fc_dim, vocab_size, tagset_size, word_vec_matrix, dropout):
+        super(BiLSTM_BiLSTM, self).__init__()
+
+        self.sentence_encoder = BiLstmSentenceEncoder(embedding_dim=embedding_dim,
+                                                      hidden_dim=hidden_dim,
+                                                      vocab_size=vocab_size,
+                                                      word_vec_matrix=word_vec_matrix)
+
+        self.sent_lstm = nn.LSTM(input_size=2*embedding_dim, hidden_size=hidden_dim, bidirectional=True, batch_first=True)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2 * hidden_dim, fc_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(fc_dim, fc_dim),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+            nn.Linear(fc_dim, tagset_size)
+        )
+
+        self.qa_score_linear = nn.Sequential(
+            nn.Linear(2*embedding_dim*4, 100),
+            nn.Linear(100, 1)
+        )
+
+    def forward(self, sentence_tuple):
+        sentence_encoder_out = self.sentence_encoder(sentence_tuple)
+
+        sent_lstm_out, _ = self.sent_lstm(sentence_encoder_out.view(1, sentence_encoder_out.shape[0], -1))
+        tag_space = self.classifier(sent_lstm_out.view(sent_lstm_out.shape[1], -1))
+
+        # tag_space = self.classifier(sentence_encoder_out)
+
+        return tag_space
+
+    def question_answer_score(self, question_tensor, answer_tensor):
+        abs_part = torch.abs(question_tensor - answer_tensor)
+        multiply_part = question_tensor * answer_tensor
+        cat_tensor = torch.cat([question_tensor, answer_tensor, abs_part, multiply_part], 0)
+        score = self.qa_score_linear(cat_tensor)
+        return score
+        # question_matrix = question_tensor.view(1, -1)
+        # answer_matrix = answer_tensor.view(1, -1)
+        #
+        # return torch.mm(question_matrix, answer_matrix.t()).view([])
+
+    # multitask loss
+    def get_loss(self, sentence_tuple, emotion_loss_func, targets):
+        sentence_encoder_out = self.sentence_encoder(sentence_tuple)
+
+        sent_lstm_out, _ = self.sent_lstm(sentence_encoder_out.view(1, sentence_encoder_out.shape[0], -1))
+        tag_space = self.classifier(sent_lstm_out.view(sent_lstm_out.shape[1], -1))
+
+        # tag_space = self.classifier(sentence_encoder_out)
+
+        emotion_loss = emotion_loss_func(tag_space, targets)
+
+        # return emotion_loss, emotion_loss, 0
+
+        # self.loss = tf.reduce_mean(tf.nn.relu(1 + self.qa_score_1 - self.qa_score_2)
+        sentence_num = len(sentence_encoder_out)
+
+        batch_loss = []
+        for i in range(sentence_num - 1):
+            sent_loss = []
+
+            sent = sentence_encoder_out[i]
+            next_sent = sentence_encoder_out[i+1]
+            true_score = self.question_answer_score(sent, next_sent)
+
+            # rand_sample = random.sample([i for i in range(sentence_num)], 10)
+
+            for j in range(sentence_num):
+                if j != i and j != i+1:
+                    false_sent = sentence_encoder_out[j]
+                    false_score = self.question_answer_score(sent, false_sent)
+                    sent_loss.append(F.relu(1 + true_score - false_score))
+
+            sent_loss_sum = sum(sent_loss)
+            sent_loss_mean = sent_loss_sum / len(sent_loss)
+            batch_loss.append(sent_loss_mean)
+
+        batch_loss_sum = sum(batch_loss)
+        batch_loss_mean = batch_loss_sum / len(batch_loss)
+
+        loss = emotion_loss + batch_loss_mean
+        # print("emotion loss: {:.3f} answer loss: {:.3f}".format(float(emotion_loss), float(batch_loss_mean)))
+
+        # loss = batch_loss_mean
+        # print("answer loss: {:.6f}".format(float(loss)))
+
+        return loss, emotion_loss, batch_loss_mean
