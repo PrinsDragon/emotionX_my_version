e3c365af28c0249ac3f5b1ebc41990b1ff5534a2
Wang Zilong <Wang_Zilong_CN@outlook.com>
2018-09-18 09:27:43 +0800

    middle of speaker embedding doc2vec part

---
+++ data/user_embedding_doc2vec.py
@@ -0,0 +1,65 @@
+import os
+
+import pandas as pd
+import numpy as np
+import csv
+import json
+import gensim
+
+def generate_user_paragragh(input_file_path, output_file_path):
+    input_file = open(input_file_path, "r")
+    json_data = json.load(input_file)
+
+    data_dict = {}
+    for dialog in json_data:
+        for sentence in dialog:
+            speaker = sentence["speaker"]
+            utterance = sentence["utterance"]
+
+            if speaker in data_dict:
+                data_dict[speaker] = data_dict[speaker] + utterance + " <END> "
+            else:
+                data_dict[speaker] = utterance + " <END> "
+
+    output_file = open(output_file_path)
+    writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
+
+    for speaker in data_dict:
+        line = [speaker, data_dict[speaker]]
+        writer.writerow(line)
+
+
+TaggedDocument = gensim.models.doc2vec.TaggedDocument
+
+class LabeledLineSentence(object):
+    def __init__(self, doc_list, labels_list):
+        self.labels_list = labels_list
+        self.doc_list = doc_list
+
+    def __iter__(self):
+        count = 0
+        for idx, doc in enumerate(self.doc_list):
+            try:
+                yield TaggedDocument(doc.split(), [self.labels_list[idx]])
+            except AttributeError:
+                count += 1
+
+def doc2vec(input_csv_path):
+
+    data = np.asarray(pd.read_csv(input_csv_path, header=None))
+    speaker_num = len(data)
+    doc_labels = [data[i][0] for i in range(speaker_num)]
+    docs = [data[i][1] for i in range(speaker_num)]
+    it = LabeledLineSentence(docs, doc_labels)
+
+    model = gensim.models.Doc2Vec(size=100, window=10, min_count=5, workers=11, alpha=0.025, min_alpha=0.025)
+    model.build_vocab(it)
+    for epoch in range(50):
+        print("Epoch: " + str(epoch))
+        model.alpha -= 0.002
+        model.min_alpha = model.alpha
+        model.train(it, total_examples=model.corpus_count, epochs=model.iter)
+        directory = "./models"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        model.save(directory + "/user_stylometric.model")
